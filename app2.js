a0a682b8bb5b2012d82facdd0fbfd8d726fd57aa
ReallyNick <nick.lindahl@gmail.com>
2022-08-16 22:19:43 -0700

    2nd commit

--- app2.js
+++ app2.js
@@ -6,6 +6,21 @@
 // }
 
 
-for (let i = 0; i < 3; i++) {
- console.log (i);
+for (let i = 0; i <= 20; i++) {
+    if( i % 3 === 0 && i % 5 === 0 ){
+        console.log(`${i} -> Frontend Simplified`);
+         }
+
+    else if( i % 3 === 0){
+        console.log(`${i} -> Frontend`);
+    }
+
+    else if( i % 5 === 0){
+        console.log(`${i} -> Simplified`);
+    }
+
+    else {
+        console.log(`${i} -> ${i}`);
+    }
+       
 }
