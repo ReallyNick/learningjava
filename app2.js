77c37f2d267708015e5cdd57d3edb54c88396014
ReallyNick <nick.lindahl@gmail.com>
2022-08-17 22:27:05 -0700

    3rd commit

--- app2.js
+++ app2.js
@@ -1,26 +1,8 @@
-// let count = 1;
 
-// while (count <= 99) {
-//     console.log (count)
-// count = count +1;
-// }
-
-
-for (let i = 0; i <= 20; i++) {
-    if( i % 3 === 0 && i % 5 === 0 ){
-        console.log(`${i} -> Frontend Simplified`);
-         }
-
-    else if( i % 3 === 0){
-        console.log(`${i} -> Frontend`);
-    }
-
-    else if( i % 5 === 0){
-        console.log(`${i} -> Simplified`);
-    }
-
-    else {
-        console.log(`${i} -> ${i}`);
-    }
-       
+// Function Definition
+function welcomePersonToFES(firstName, lastName){    
+    console.log (`Welcome to Frontend Simplified, ${firstName} ${lastName}`)
 }
+
+// call the function
+welcomePersonToFES(`Nick`, `Lindahl`);
